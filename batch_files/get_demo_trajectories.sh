b0bca0fd4d3b189cc3cd42f8f5b09552a0f2da8b
Ashraf Ibraheem <ibraheemashraf10@gmail.com>
2025-04-28 13:43:47 -0400

    Save gesture script & batch file

---
+++ batch_files/get_demo_trajectories.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=Fine_Tune_gr00t  # Job name
+#SBATCH --ntasks=1                  # Number of tasks (processes)
+#SBATCH --gpus-per-task=1           # Number of CPU cores per task
+#SBATCH --mem=32G  # Request more memory (adjust as needed)
+#SBATCH --constraint=gpul40s        # Type of gpu required for task
+#SBATCH --time=03:00:00             # Time limit hrs:min:sec
+#SBATCH --output=batch_files/get_demo_trajectories_output_%j.log      # Standard output log
+# Note: The above comments are supposed to be like this, its how the compiler registers flags for sbatch
+
+# TO RUN THIS SCRIPT DO: sbatch -A axb1653 -p gpu --constraint=gpul40s bash_file_name
+
+# Load any modules if needed
+# module load python
+module load Miniconda3
+eval "$(conda shell.bash hook)"
+conda activate gr00t
+# Run your Python script
+python -u scripts/get_trajectories.py -em new_embodiment -dc baxter_arms -d novideo_data -ckpt models/finetuned-baxter-model
+python -u scripts/get_trajectories.py -em new_embodiment -dc baxter_arms -d novideo_data -ckpt models/finetuned-baxter-model
+python -u scripts/get_trajectories.py -em new_embodiment -dc baxter_arms -d novideo_data -ckpt models/finetuned-baxter-model
+python -u scripts/get_trajectories.py -em new_embodiment -dc baxter_arms -d novideo_data -ckpt models/finetuned-baxter-model
+conda deactivate
